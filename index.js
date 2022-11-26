3be41e2a7f8d78b5544c586ca080b719cd0db240
tracykagai-2022 <tracy.kagai@student.moringaschool.com>
2022-11-26 10:18:42 +0300

    Done

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
+const num1 = 31;
+const num2 = 2;
+const num3 = 24;
+const num4 = 5;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
+let mod = num3 % num4;
+let max =Math.max(1, 6, 8, 20);
 
-// Write your code here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
